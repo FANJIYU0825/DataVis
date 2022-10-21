d89ee43198be171febf22a0419ee90223e62f569
FANFNA <55987247+FANJIYU0825@users.noreply.github.com>
2022-10-21 09:51:22 +0800

    Setting the them of the frame  use the way then . /n Please do not use then lonely will get the race condition over write you code. It will waste your time. An oppinance will you a lot

--- Hw4/map.js
+++ Hw4/map.js
@@ -1,4 +1,4 @@
-const FWith = 600,
+const FWith = 1000,
   FHeight = 400;
 const FLeftTopX = 10,
   FLeftTopY = 10;
@@ -29,6 +29,45 @@ const g2 = svg2
     "transform",
     `translate(${FLeftTopX + MARGIN.LEFT}, ${FLeftTopY + MARGIN.TOP})`
   );
+  const svg3 = d3
+  .select("#area1")
+  .append("svg")
+  .attr("width", FWith - 150)
+  .attr("height", FHeight);
+
+const g3 = svg3
+  .append("g")
+  .attr(
+    "transform",
+    `translate(${FLeftTopX + MARGIN.LEFT}, ${FLeftTopY + MARGIN.TOP}) `
+  );
+  const svg4 = d3
+  .select("#area1")
+  .append("svg")
+  .attr("width", FWith - 150)
+  .attr("height", FHeight);
+
+const g4 = svg4
+  .append("g")
+  .attr(
+    "transform",
+    `translate(${FLeftTopX + MARGIN.LEFT}, ${FLeftTopY + MARGIN.TOP})`
+  );
+  const svg5 = d3
+  .select("#area1")
+  .append("svg")
+  .attr("width", FWith - 150)
+  .attr("height", FHeight);
+
+const g5 = svg5
+  .append("g")
+  .attr(
+    "transform",
+    `translate(${FLeftTopX + MARGIN.LEFT}, ${FLeftTopY + MARGIN.TOP})`
+  );
+
+
+  
 
 d3.csv("data/NBA.csv", d3.autoType).then(function (data) {
   d3.json("data/us-states.json")
@@ -65,12 +104,107 @@ d3.csv("data/NBA.csv", d3.autoType).then(function (data) {
       g1.append("g").call(yAxisCall)
       g1.append("g")
       .call(xAxisCall).attr("transform", "translate(0,"+HEIGHT+")")
+    })
+    .then(function () {
+      g1.selectAll('text').remove()
       
-     
-    });
+    }).then(function () {
+      g3.append("text")
+        .attr("x", WIDTH / 2)
+        .attr("y", HEIGHT + 20)
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .text("umapX");
+
+      //y
+      const y = d3
+        .scaleLinear()
+        .domain([d3.min(data, (d) => d.umapY), d3.max(data, (d) => d.umapY)])
+        .range([HEIGHT, 0]);
+      const x = d3
+        .scaleLinear()
+        .domain([d3.min(data, (d) => d.umapY), d3.max(data, (d) => d.umapY)])
+        .range([0, WIDTH]);
+      // Y label
+      g3.append("text")
+        .attr("x", -(HEIGHT / 2))
+        .attr("y", -20)
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("umapY");
+      const yAxisCall = d3.axisLeft(y);
+      const xAxisCall = d3.axisBottom(x);
+      g3.append("g").call(yAxisCall)
+      g3.append("g")
+      .call(xAxisCall).attr("transform", "translate(0,"+HEIGHT+")")
+    }).then(function () {
+      g4.append("text")
+        .attr("x", WIDTH / 2)
+        .attr("y", HEIGHT + 20)
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .text("umapX");
+
+      //y
+      const y = d3
+        .scaleLinear()
+        .domain([d3.min(data, (d) => d.umapY), d3.max(data, (d) => d.umapY)])
+        .range([HEIGHT, 0]);
+      const x = d3
+        .scaleLinear()
+        .domain([d3.min(data, (d) => d.umapY), d3.max(data, (d) => d.umapY)])
+        .range([0, WIDTH]);
+      // Y label
+      g4.append("text")
+        .attr("x", -(HEIGHT / 2))
+        .attr("y", -20)
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("umapY");
+      const yAxisCall = d3.axisLeft(y);
+      const xAxisCall = d3.axisBottom(x);
+      g4.append("g").call(yAxisCall)
+      g4.append("g")
+      .call(xAxisCall).attr("transform", "translate(0,"+HEIGHT+")")
+    }).then(function () {
+      g5.append("text")
+        .attr("x", WIDTH / 2)
+        .attr("y", HEIGHT + 20)
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .text("umapX");
+
+      //y
+      const y = d3
+        .scaleLinear()
+        .domain([d3.min(data, (d) => d.umapY), d3.max(data, (d) => d.umapY)])
+        .range([HEIGHT, 0]);
+      const x = d3
+        .scaleLinear()
+        .domain([d3.min(data, (d) => d.umapY), d3.max(data, (d) => d.umapY)])
+        .range([0, WIDTH]);
+      // Y label
+      g5.append("text")
+        .attr("x", -(HEIGHT / 2))
+        .attr("y", -20)
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("umapY");
+      const yAxisCall = d3.axisLeft(y);
+      const xAxisCall = d3.axisBottom(x);
+      g5.append("g").call(yAxisCall)
+      g5.append("g")
+      .call(xAxisCall).attr("transform", "translate(0,"+HEIGHT+")")
+    })
+   
 });
 
-function scatter() {}
+function draw_scatter() {
+
+}
 
 /*
 
@@ -79,8 +213,8 @@ function drawUsa(Usa, data) {
   //usmap
   var projection = d3.geoMercator().fitExtent(
     [
-      [0, 0],
-      [WIDTH, HEIGHT],
+      [-305, 20],
+      [WIDTH+160, HEIGHT+180],
     ],
     Usa
   );
@@ -94,7 +228,7 @@ function drawUsa(Usa, data) {
     .attr("stroke", "white")
     .attr("fill", "steelblue")
     .attr("d", geoGenerator)
-    .attr("transform", "scale(.6,.6)");
+    
 
   data.forEach(function (d) {
     d.lon = Number(d.lon);
@@ -116,13 +250,13 @@ function drawUsa(Usa, data) {
       .text(function (d) {
         return d.team_abbreviation;
       })
-      .attr("transform", "scale(.6,.6)");
+      
     g2.append("circle")
       .attr("id", "citycircle")
       .attr("cx", projection([d.lon, d.lat])[0])
       .attr("cy", projection([d.lon, d.lat])[1])
       .attr("fill", "red")
       .attr("r", 10)
-      .attr("transform", "scale(.6,.6)");
+      
   });
 }
